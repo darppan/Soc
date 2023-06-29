b39214b1f610683d696c59063291669e20b91753
Darppan MV <darppan524@gmail.com>
2023-06-29 12:49:02 +0530

    changes after json

--- Header/bvh.h
+++ Header/bvh.h
@@ -2,7 +2,7 @@
 #define BVH_H
 
 #include "rtweekend.h"
-#include "algorithm"
+#include <algorithm>
 
 #include "hittable.h"
 #include "hittable_list.h"
@@ -10,44 +10,16 @@
 
 class bvh_node : public hittable {
     public:
-        bvh_node();
+        bvh_node(){};
 
         bvh_node(const hittable_list& list, double time0, double time1)
             : bvh_node(list.objects, 0, list.objects.size(), time0, time1)
         {}
 
-        bvh_node(
-            const std::vector<shared_ptr<hittable>>& src_objects,
-            size_t start, size_t end, double time0, double time1);
-
         virtual bool hit(
             const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
         virtual bool bounding_box(double time0, double time1, aabb& output_box) const override;
-
-        inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis)
-        {
-        aabb box_a;
-        aabb box_b;
-
-        if (!a->bounding_box(0,0, box_a) || !b->bounding_box(0,0, box_b))
-            std::cerr << "No bounding box in bvh_node constructor.\n";
-
-        return box_a.min().e[axis] < box_b.min().e[axis];
-        }
-
-
-        bool box_x_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-            return box_compare(a, b, 0);
-        }
-
-        bool box_y_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-            return box_compare(a, b, 1);
-        }
-
-        bool box_z_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-            return box_compare(a, b, 2);
-        }
 
     public:
         shared_ptr<hittable> left;
@@ -70,18 +42,20 @@ bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) co
     return hit_left || hit_right;
 }
 
-bvh_node::bvh_node(
+bvh_node :: bvh_node(
     std::vector<shared_ptr<hittable>>& src_objects,
     size_t start, size_t end, double time0, double time1
 ) {
-    auto objects = src_objects;
+    auto objects = src_objects; 
 
     int axis = random_int(0,2);
-    auto comparator = (axis == 0) ? box_x_compare
-                    : (axis == 1) ? box_y_compare
-                                  : box_z_compare;
+    
 
     size_t object_span = end - start;
+
+     auto comparator = (axis == 0) ? box_x_compare
+                    : (axis == 1) ? box_y_compare
+                                  : box_z_compare;
 
     if (object_span == 1) {
         left = right = objects[start];
@@ -111,4 +85,28 @@ bvh_node::bvh_node(
     box = surrounding_box(box_left, box_right);
 }
 
+
+
+inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
+    aabb box_a;
+    aabb box_b;
+
+    if (!a->bounding_box(0,0, box_a) || !b->bounding_box(0,0, box_b))
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+
+    return box_a.min().e[axis] < box_b.min().e[axis];
+}
+
+
+bool box_x_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 0);
+}
+
+bool box_y_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 1);
+}
+
+bool box_z_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 2);
+}
 #endif
